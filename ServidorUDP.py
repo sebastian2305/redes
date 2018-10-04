daf00df7466c15a471f06ccf2d0234a31529ec3e
Sebastian Cespedes Otalora <s.cespedes10@sis.virtual.uniandes.edu.co>
2018-10-04 10:14:33 -0500

    Modificacion Servidor Cliente

--- ServidorUDP.py
+++ ServidorUDP.py
@@ -22,18 +22,6 @@ class Client(Thread):
         def run(self):
             # data received from client
             m = hashlib.sha256();
-            # data = self.conn.recv(2048)
-            # data1 = data.decode('utf-8')
-
-            # if data1 != 'Preparado para recibir':
-            # print('El Cliente no esta preparado para recibir')
-            # print_lock.release()
-            # elif data1 == 'Preparado para recibir':
-            # print('Cliente preparado para recibir')
-
-            # s.acquire()
-
-            # Tamanio del archivo
             data = self.conn.recv(2048)
             datoTamanio = data.decode('utf-8')
 
@@ -56,34 +44,16 @@ class Client(Thread):
             print('En Cliente recibio el archivo correctamente')
             self.conn.close()
 
-
 def Main():
     global ConnectedCount
     global numClientes
     host = ''
-
-    # reverse a port on your computer
-    # in our case it is 12345 but it
-    # can be anything
     port = 222
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     s.bind((host, port))
-    # while True:
-    # print("socket binded to post", port)
-
-    # put the socket into listening mode
-
     print("socket is listening")
 
-    # a forever loop until client wants to exit
-
     while numClientes < ClientCount:
-
-        # establish connection with client
-        c, addr = s.accept()
-        # lock acquired by client
-        # print_lock.acquire()
-        # print('Connected to :', addr[0], ':', addr[1])
 
         # esperar a recibir "Preparado para recibir"
         resp = c.recv(1024)
