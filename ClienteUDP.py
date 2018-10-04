37b6a9c3b5b43b10a2e07c4fbb35150571b55227
Sebastian Cespedes Otalora <s.cespedes10@sis.virtual.uniandes.edu.co>
2018-10-04 09:56:53 -0500

    Modificacion Servidor Cliente

--- ClienteUDP.py
+++ ClienteUDP.py
@@ -1,18 +1,105 @@
 import socket
-import sys
+import os
+import hashlib
+import time
+from datetime import datetime
 
-arglen = len(sys.argv)
-if arglen < 3:
-    print('Por favor correrlo con los siguientes parametros ClienteUDP.py <ip_address> <numbers>')
-    exit()
 
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-port = 222
-addr = sys.argv[1]
-data = str()
-data = data + sys.argv[2]
-for i in range(3, len(sys.argv)):
-    data = data + ':' + sys.argv[i]
-s.sendto(data, (addr, port))
-data, addr = s.recvfrom(1024)
-print data
+def millis(start_time):
+    dt = datetime.now() - start_time
+    ms = (dt.days * 24 * 60 * 60 + dt.seconds) * 1000 + dt.microseconds / 1000.0
+    return ms
+
+
+def Main():
+    # local host IP '127.0.0.1'
+    host = '127.0.0.1'
+
+    # Define the port on which you want to connect
+    port = 222
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+    # connect to server on local computer
+    s.connect((host, port))
+
+    # Mensaje preparado para recibir
+    message_prep = "Preparado para recibir"
+
+    # Mensaje recibido
+    message_rec = "Recibido"
+
+    # Mensaje recibido no integro
+    message_rec_in = "Mensaje no recibido"
+    # Archivo log
+    logClient = open('log.txt', 'w')
+    logClient.write('Fecha y hora de la prueba\n')
+    logClient.write(time.asctime())
+
+    # message preparado para recibir
+    s.send(message_prep.encode('utf-8'))
+    ans = input('Que archivo quiere recibir(250MiB o 500MiB)? 250/500\n')
+    s.send(str(ans).encode('utf-8'))
+    # Hash
+    dataHash = s.recv(2048)
+    if ans == 250:
+        # Data
+        print('Entre a 250')
+        with open('output250.webm', 'wb') as f:
+            logClient.write('\nNombre del archivo\n')
+            logClient.write('output250.webm')
+            logClient.write('\nDireccion del cliente\n')
+            logClient.write(socket.gethostbyname(socket.gethostname()))
+
+            start_time = datetime.now()
+
+            data = s.recv(4096)
+            while data:
+                f.write(data)
+                data = s.recv(4096)
+            logClient.write('\nTiempo APROXIMADO de llegada\n')
+            logClient.write(str(millis(start_time) + 254.263))
+            logClient.write('Tamanio del archivo\n')
+            logClient.write(str(os.stat('output250.webm').st_size))
+        with open('output250.webm', 'rb') as f1:
+            m = hashlib.sha256();
+            contenido = f1.read()
+            m.update(contenido);
+            dig = m.digest();
+    if ans == 500:
+        print('Entre a 500')
+        # Data
+        with open('output500.mp4', 'wb') as f:
+            logClient.write('\nNombre del archivo\n')
+            logClient.write('output500.mp4')
+            logClient.write('\nDireccion del cliente\n')
+            logClient.write(socket.gethostbyname(socket.gethostname()))
+
+            start_time = datetime.now()
+            data = s.recv(4096)
+            while data:
+                f.write(data)
+                data = s.recv(4096)
+            logClient.write('\nTiempo APROXIMADO de llegada\n')
+            logClient.write(str(millis(start_time) + (254.263 * 3)))
+            logClient.write('\nTamanio del archivo\n')
+            logClient.write(str(os.stat('output500.mp4').st_size))
+        with open('output500.mp4', 'rb') as f1:
+            m = hashlib.sha256();
+            contenido = f1.read()
+            m.update(contenido);
+            dig = m.digest();
+        if dig == dataHash:
+            logClient.write('\nEl archivo llego correctamente\n')
+            print('Recibido exitosamente')
+        else:
+            logClient.write('\nEl archivo NO llego correctamente\n')
+            print('No cumple con la integridad:')
+
+            # close the connection
+    logClient.close()
+    s.close()
+
+
+if __name__ == '__main__':
+    Main()
--- ServidorUDP.py
+++ ServidorUDP.py
@@ -1,6 +1,115 @@
 #UDPserver.py
 #!/usr/bin/python
 import socket
+import hashlib
+from datetime import datetime
+from threading import Thread
+
+ClientCount = int(input("Especifique cuantos clientes quiere en simultaneo\n" ))
+ConnectedClients = []
+numClientes = 0
+def millis(start_time):
+    dt = datetime.now() - start_time
+    ms = (dt.days * 24 * 60 * 60 + dt.seconds) * 1000 + dt.microseconds / 1000.0
+    return ms
+
+class Client(Thread):
+        def __init__(self, conn, addr):
+            Thread.__init__(self)
+            self.conn = conn
+            self.addr = addr
+
+        def run(self):
+            # data received from client
+            m = hashlib.sha256();
+            # data = self.conn.recv(2048)
+            # data1 = data.decode('utf-8')
+
+            # if data1 != 'Preparado para recibir':
+            # print('El Cliente no esta preparado para recibir')
+            # print_lock.release()
+            # elif data1 == 'Preparado para recibir':
+            # print('Cliente preparado para recibir')
+
+            # s.acquire()
+
+            # Tamanio del archivo
+            data = self.conn.recv(2048)
+            datoTamanio = data.decode('utf-8')
+
+            # reverse the given string from client
+            if datoTamanio == '250':
+                with open('250.webm', 'rb') as dd:
+                    contenido = dd.read()
+                    m.update(contenido)
+                    dig = m.digest()
+                    self.conn.send(dig)
+                    self.conn.send(contenido)
+
+            else:
+                with open('500.mp4', 'rb') as dd:
+                    contenido = dd.read()
+                    m.update(contenido)
+                    dig = m.digest()
+                    self.conn.send(dig)
+                    self.conn.send(contenido)
+            print('En Cliente recibio el archivo correctamente')
+            self.conn.close()
+
+
+def Main():
+    global ConnectedCount
+    global numClientes
+    host = ''
+
+    # reverse a port on your computer
+    # in our case it is 12345 but it
+    # can be anything
+    port = 222
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.bind((host, port))
+    # while True:
+    # print("socket binded to post", port)
+
+    # put the socket into listening mode
+    s.listen(25)
+    print("socket is listening")
+
+    # a forever loop until client wants to exit
+
+    while numClientes < ClientCount:
+
+        # establish connection with client
+        c, addr = s.accept()
+        # lock acquired by client
+        # print_lock.acquire()
+        # print('Connected to :', addr[0], ':', addr[1])
+
+        # esperar a recibir "Preparado para recibir"
+        resp = c.recv(1024)
+        if (resp.decode("utf-8") == "Preparado para recibir"):
+            # create a new thread and add it to the created list
+            c = Client(c, addr)
+            ConnectedClients.append(c)
+            numClientes += 1
+
+    print("Llegaron todos los clientes")
+    for x in ConnectedClients:
+        x.start()
+    s.close()
+
+
+if __name__ == '__main__':
+    Main()
+
+
+
+
+
+
+
+
+
 s=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 s.bind(('127.0.0.1',222))
 while True:
